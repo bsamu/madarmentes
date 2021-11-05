b6256fdb2c402bbfda24bd258dfd24f1a832576d
IstvanPongracz <pongracz88@gmail.com>
2021-11-05 12:00:01 +0100

    navbar background

---
+++ mainpage/script.js
@@ -0,0 +1,14 @@
+var myNav = document.getElementById('mynav');
+window.onscroll = function loadEvent() { 
+    "use strict";
+    if (document.body.scrollTop >= 200 ) {
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
+    }
+};
+
+window.addEventListener("load", loadEvent);
