aad11aaa3962aca8da825d5eb726ae2a2475c750
IstvanPongracz <pongracz88@gmail.com>
2021-11-05 15:23:57 +0100

    main page main changes

--- mainpage/script.js
+++ mainpage/script.js
@@ -1,14 +1,2 @@
-var myNav = document.getElementById('mynav');
-window.onscroll = function loadEvent() { 
-    "use strict";
-    if (document.body.scrollTop >= 200 ) {
-        myNav.classList.add("nav-colored");
-        myNav.classList.remove("nav-transparent");
-    } 
-    else {
-        myNav.classList.add("nav-transparent");
-        myNav.classList.remove("nav-colored");
-    }
-};
 
 window.addEventListener("load", loadEvent);
